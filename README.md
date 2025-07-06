e82f030c838701670f691990a744bf06de09e638
EwoutBergsma <43827037+EwoutBergsma@users.noreply.github.com>
2025-07-06 22:32:42 +0200

    playng around with some LEDs, created my own function. i.e. refreshing the basics

---
+++ README.md
@@ -0,0 +1,7 @@
+I am practising my nRF connect SDK skills by creating a simple application.
+
+Steps I took so far:
+1. Created a new project using the nRF Connect SDK, then use `create a blank application`.
+1. Created a build configuration for the application, using `nRF54L15dk/nRF54L15/cpu_app` it does give a warning about security. It proposes to use the `/ns` version, but that did not build. 
+    - Flashing then worked, but we have nothing to show for it.
+1.  
--- src/main.c
+++ src/main.c
@@ -1,6 +1,40 @@
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+static const struct gpio_dt_spec led_0 = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
+static const struct gpio_dt_spec led_1 = GPIO_DT_SPEC_GET(DT_ALIAS(led1), gpios);
+static const struct gpio_dt_spec led_2 = GPIO_DT_SPEC_GET(DT_ALIAS(led2), gpios);
+static const struct gpio_dt_spec led_3 = GPIO_DT_SPEC_GET(DT_ALIAS(led3), gpios);
+
+
+int setup_led(const struct gpio_dt_spec *led)
+{
+        if (!device_is_ready(led->port)) return -1;  // Device not ready
+        if (gpio_pin_configure_dt(led, GPIO_OUTPUT_ACTIVE) < 0) return -1;  // Configuration failed
+        if (gpio_pin_toggle_dt(led) < 0) return -1;  // Toggle failed
+
+        return 0;  // Success
+}
 
 int main(void)
 {
+         if (setup_led(&led_0) < 0 || setup_led(&led_1) < 0 || setup_led(&led_2) < 0 || setup_led(&led_3) < 0) {
+                return -1; 
+        }
+
+
+        for (int i = 0; i < 3; i++) {
+                gpio_pin_set_dt(&led_0,1); // Turns on LED 
+                gpio_pin_set_dt(&led_1,1); // Turns on LED
+                gpio_pin_set_dt(&led_2,1); // Turns on LED
+                gpio_pin_set_dt(&led_3,1); // Turns on LED
+                k_msleep(1000); // Wait for 1 second
+                gpio_pin_set_dt(&led_0,0); // Turns off LED
+                gpio_pin_set_dt(&led_1,0); // Turns off LED
+                gpio_pin_set_dt(&led_2,0); // Turns off LED
+                gpio_pin_set_dt(&led_3,0); // Turns off LED
+                k_msleep(1000); // Wait for 1 second
+        }
+
         return 0;
 }
